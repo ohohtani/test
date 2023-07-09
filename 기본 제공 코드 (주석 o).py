6cfeabca1058eac8fe694e05f91e4f512169896f
ohohtani <138994924+ohohtani@users.noreply.github.com>
2023-07-10 00:38:36 +0900

    기본 제공 코드 (주석 o).py

--- 기본 제공 코드 (주석 o).py
+++ 기본 제공 코드 (주석 o).py
@@ -1,4 +1,5 @@
 # 헷갈릴까봐 주석을 달아보았어요. 참고하세요
+
 import os    # 파일 디렉토리 관련
 import cv2  
 import pandas as pd
@@ -13,28 +14,36 @@ from tqdm import tqdm    # 진행상황을 시각화 해준다
 import albumentations as A   # 이미지 처리 작업을 위한 다양한 도메인 지원
 from albumentations.pytorch import ToTensorV2   # albumentations 를 통해 수행된 이미지 변환을 텐서 형식으로 바꿀 수 있다
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # GPU를 사용할 수 있는 경우에는 사용하고, 그렇지 않으면 CPU를 사용한다 (GPU 사용시 작업수행이 굉장히 빠른데 이거하려면 엔비디아 드라이브 다운받아야 되고
-                                                                       # 꽤나 귀찮지만 여유가 있다면 하면 더 좋긴하다 (본인스펙 RTX 3060에  cpu 라이젠 7 5800X인데 드라이브 다운로드 한시간 넘게 걸렸음)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # GPU를 사용할 수 있는 경우에는 사용하고, 그렇지 않으면 CPU를 사용한다 (GPU 사용시 작업수행이 굉장히 빠른데 이거하려면 엔비디아 드라이브 다운받아야 돼서
+                                                                       # 꽤나 귀찮지만 여유가 있다면 하면 더 좋긴하다. 본인스펙 RTX 3060에  cpu 라이젠 7 5800X인데 드라이브 다운로드 한시간 넘게 걸렸음)
 
 
 # RLE 디코딩 함수 , RLE는 그냥 데이터 압축 방식 중 하나라고 생각하면 된다
 def rle_decode(mask_rle, shape):    # rle_decode라는 함수를 정의한다. 인자로는 rle 방식으로 압축된 mask와 원래 형태의 이미지(shape)를 받는다.
-    s = mask_rle.split()   # maks_rle(rle 방식으로 압축된 마스크)를 공백을 기준으로 분할하여 s에 저장한다.
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
-    ends = starts + lengths
-    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 1
-    return img.reshape(shape)
+    s = mask_rle.split()            # maks_rle(rle 방식으로 압축된 마스크)를 공백을 기준으로 분할하여 s 리스트에 저장한다. (split이 그런 역할임 ㅇㅇ)
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]   # s 리스트에서 짝수 인덱스와 홀수 인덱스를 분리하여 각각 starts에 위치 lengths에 길이로 저장한다.
+                                                                                     # np.asarray 는 시작 위치와 길이를 정수 배열로 변환하는 역할을 해준다.
+    starts -= 1  # RLE 에서는 인덱스가 1부터 시작한다고 한다. 배열의 인덱스는 0부터 시작하기 때문에 배열의 모든 요소에 1을 빼준다.
+    ends = starts + lengths  # 끝 위치 = 시작 위치 + 길이
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)  # 이미지 1차원 배열 생성
+    for lo, hi in zip(starts, ends):     # starts 와 ends 배열을 순회하면서 img 배열에서 해당하는 구간을 1로 설정한다.  ? 무슨 말일까
+        img[lo:hi] = 1                   
+    return img.reshape(shape)            # 아무튼 img 재열을 주어진 형태(reshape)로 다시 변형하여 복원된 마스크를 반환한다.
 
-# RLE 인코딩 함수
-def rle_encode(mask):
-    pixels = mask.flatten()
+# RLE 인코딩 함수 , 디코딩을 했으니 인코딩을 해줘야한다. 참고로    [디코딩 = 압축된 데이터를 원래 형태로 해독, 인코딩 = 원래의 데이터를 다른 형식으로 변환]
+def rle_encode(mask):  # 그니까 인코딩 정의에 의해서 인자는 그냥 원래 데이터를 받는 거임 (아마?)
+    pixels = mask.flatten()                              #  mask 배열을 1차원으로 펼쳐 pixels에 저장한다. -> 마스크의 모든 값을 하나의 1차원 배열로 변환한다. (1차원이 돼야 RLE 압축이 된다)
     pixels = np.concatenate([[0], pixels, [0]])
     runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
     runs[1::2] -= runs[::2]
     return ' '.join(str(x) for x in runs)
+
+
+
+
+
+# 열심히 읽어주셔서 감사하나 사실 여기까지는 수정할 필요가 없다. 우리는 성능 향상이 목적이기 때문에 이정도만 이해하고 밑을 수정해야 한다.
+
 
 
 class SatelliteDataset(Dataset):
