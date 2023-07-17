7a3742c8e47e8cd0a0b8047b5fd83a951dd768df
syyy12 <139008659+syyy12@users.noreply.github.com>
2023-07-17 23:50:58 +0900

    Update 김동하 Deeplab.py

--- 김동하 Deeplab.py
+++ 김동하 Deeplab.py
@@ -1,58 +1,50 @@
-import os    
-import cv2  
-import pandas as pd
-import numpy as np
-
-import torch 
+import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader    
-from torchvision import transforms   
+import torch.nn.functional as F
+from torchvision import models, transforms
+from torch.utils.data import Dataset, DataLoader
 
-from tqdm import tqdm    
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+def rle_decode(mask_rle, shape):    # rle_decode라는 함수를 정의한다. 인자로는 rle 방식으로 압축된 mask와 원래 형태의 이미지(shape)를 받는다.
+    s = mask_rle.split()            # maks_rle(rle 방식으로 압축된 마스크)를 공백을 기준으로 분할하여 s 리스트에 저장한다. (split이 그런 역할임 ㅇㅇ)
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]   # s 리스트에서 짝수 인덱스와 홀수 인덱스를 분리하여 각각 starts에 위치 lengths에 길이로 저장한다.
+                                                                                     # np.asarray 는 시작 위치와 길이를 정수 배열로 변환하는 역할을 해준다.
+    starts -= 1  # RLE 에서는 인덱스가 1부터 시작한다고 한다. 배열의 인덱스는 0부터 시작하기 때문에 배열의 모든 요소에 1을 빼준다.
+    ends = starts + lengths  # 끝 위치 = 시작 위치 + 길이
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)  # 이미지 1차원 배열 생성
+    for lo, hi in zip(starts, ends):     # starts 와 ends 배열을 순회하면서 img 배열에서 해당하는 구간을 1로 설정(해당 run의 위치를 표시하는 것)한다.  ? 무슨 말일까 : lo, hi 는 각 run의 시작위치와 끝 위치를 나타냄
+        img[lo:hi] = 1                   
+    return img.reshape(shape)            # 아무튼 img 배열을 주어진 형태(reshape)로 다시 변형하여 복원된 마스크를 반환한다.
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-# RLE 디코딩 함수
-def rle_decode(mask_rle, shape):
-    s = mask_rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
-    ends = starts + lengths
-    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 1
-    return img.reshape(shape)
-
-# RLE 인코딩 함수
-def rle_encode(mask):
-    pixels = mask.flatten()
+# RLE 인코딩 함수 , 디코딩을 했으니 인코딩을 해줘야한다. 참고로    [디코딩 = 압축된 데이터를 원래 형태로 해독, 인코딩 = 원래의 데이터를 다른 형식으로 변환]
+def rle_encode(mask):  # 그니까 인코딩 정의에 의해서 인자는 그냥 원래 데이터를 받는 거임 (아마?)
+    pixels = mask.flatten()                              #  mask 배열을 1차원으로 펼쳐 pixels에 저장한다. -> 마스크의 모든 값을 하나의 1차원 배열로 변환한다. (1차원이 돼야 RLE 압축이 된다)
     pixels = np.concatenate([[0], pixels, [0]])
     runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
     runs[1::2] -= runs[::2]
     return ' '.join(str(x) for x in runs)
 
-
-# 이미지 세그멘테이션을 위한 데이터셋 클래스 정의
-class SegmentationDataset(Dataset):
-    def __init__(self, csv_file, transform=None):
+# Define the SatelliteDataset class
+class SatelliteDataset(Dataset):
+    def __init__(self, csv_file, transform=None, infer=False):
         self.data = pd.read_csv(csv_file)
         self.transform = transform
+        self.infer = infer
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        img_path = self.data.iloc[idx, 1]  # 이미지 파일 경로는 1번째 열에 저장되어 있다고 가정합니다.
-        mask_rle = self.data.iloc[idx, 2]  # 마스크 RLE 데이터는 2번째 열에 저장되어 있다고 가정합니다.
-
+        img_path = self.data.iloc[idx, 1]
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        mask = rle_decode(mask_rle, (image.shape[0], image.shape[1]))  # 마스크 RLE 데이터를 디코딩하여 이미지 형태로 변환합니다.
+        if self.infer:
+            if self.transform:
+                image = self.transform(image=image)['image']
+            return image
+
+        mask_rle = self.data.iloc[idx, 2]
+        mask = rle_decode(mask_rle, (image.shape[0], image.shape[1]))
 
         if self.transform:
             augmented = self.transform(image=image, mask=mask)
@@ -61,9 +53,40 @@ class SegmentationDataset(Dataset):
 
         return image, mask
 
+# Define the DeepLab model
+class DeepLab(nn.Module):
+    def __init__(self, num_classes):
+        super(DeepLab, self).__init__()
+        self.num_classes = num_classes
 
+        # Load the pre-trained ResNet-101 model
+        resnet = models.resnet101(pretrained=True)
 
-# 이미지 변환을 위한 Albumentations 변환 정의
+        # Extract the layers from the ResNet model
+        self.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+
+        # Atrous convolutional layer with dilation rate 2
+        self.atrous_convolution = nn.Conv2d(2048, 1024, kernel_size=3, stride=1, padding=2, dilation=2)
+
+    def forward(self, x):
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.atrous_convolution(x)
+
+        # Upsample the output to match the input size
+        x = F.interpolate(x, size=(x.size(2) * 8, x.size(3) * 8), mode='bilinear', align_corners=True)
+
+        return x
+
+# Define the data transformations
 transform = A.Compose(
     [
         A.Resize(224, 224),
@@ -72,167 +95,61 @@ transform = A.Compose(
     ]
 )
 
-# 학습 데이터셋 및 데이터 로더 생성
-train_dataset = SegmentationDataset(csv_file='./train.csv', transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
+# Load the training data
+dataset = SatelliteDataset(csv_file='./train.csv', transform=transform)
+dataloader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=4)
 
+# Initialize the model and move it to the GPU if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = DeepLab(num_classes=1).to(device)
 
+# Define the loss function and optimizer
+criterion = torch.nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-
-class DeepLab(nn.Module):
-    def __init__(self):
-        super(DeepLab, self).__init__()
-
-        self.dconv_down1 = DoubleConv(3, 64)
-        self.dconv_down2 = DoubleConv(64, 128)
-        self.dconv_down3 = DoubleConv(128, 256)
-        self.dconv_down4 = DoubleConv(256, 512)
-
-        self.maxpool = nn.MaxPool2d(2)
-
-        self.aspp = ASPP(512, 256)
-
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-
-        self.dconv_up3 = DoubleConv(256 + 512, 256)
-        self.dconv_up2 = DoubleConv(128 + 256, 128)
-        self.dconv_up1 = DoubleConv(128 + 64, 64)
-
-        self.conv_last = nn.Conv2d(64, 1, 1)
-
-    def forward(self, x):
-        conv1 = self.dconv_down1(x)
-        x = self.maxpool(conv1)
-
-        conv2 = self.dconv_down2(x)
-        x = self.maxpool(conv2)
-
-        conv3 = self.dconv_down3(x)
-        x = self.maxpool(conv3)
-
-        x = self.dconv_down4(x)
-
-        x = self.aspp(x)
-
-        x = self.upsample(x)
-        x = torch.cat([x, conv3], dim=1)
-
-        x = self.dconv_up3(x)
-        x = self.upsample(x)
-        x = torch.cat([x, conv2], dim=1)
-
-        x = self.dconv_up2(x)
-        x = self.upsample(x)
-        x = torch.cat([x, conv1], dim=1)
-
-        x = self.dconv_up1(x)
-
-        out = self.conv_last(x)
-
-        return out
-
-
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(DoubleConv, self).__init__()
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
-
-
-class ASPP(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(ASPP, self).__init__()
-
-        rates = [1, 6, 12, 18]
-
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, dilation=rates[0], padding=rates[0])
-        self.conv3 = nn.Conv2d(in_channels, out_channels, kernel_size=3, dilation=rates[1], padding=rates[1])
-        self.conv4 = nn.Conv2d(in_channels, out_channels, kernel_size=3, dilation=rates[2], padding=rates[2])
-        self.conv5 = nn.Conv2d(in_channels, out_channels, kernel_size=3, dilation=rates[3], padding=rates[3])
-
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-
-        self.conv_bn_relu = nn.Sequential(
-            nn.Conv2d(out_channels * 6, out_channels, kernel_size=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        conv1 = self.conv1(x)
-        conv2 = self.conv2(x)
-        conv3 = self.conv3(x)
-        conv4 = self.conv4(x)
-        conv5 = self.conv5(x)
-
-        pooled = self.avg_pool(x)
-        pooled = torch.nn.functional.interpolate(pooled, size=(x.size(2), x.size(3)), mode='bilinear', align_corners=True)
-
-        output = torch.cat([conv1, conv2, conv3, conv4, conv5, pooled], dim=1)
-
-        output = self.conv_bn_relu(output)
-
-        return output
-
-
-
-
-model = DeepLab()
-model = model.to(device)  # 모델을 GPU로 전송합니다.
-
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-criterion = nn.BCEWithLogitsLoss()
-
+# Train the model for 10 epochs
+# Train the model for 10 epochs
 for epoch in range(10):
     model.train()
     epoch_loss = 0
-
-    for images, masks in tqdm(train_loader):
-        images = images.to(device)
-        masks = masks.to(device)
+    for images, masks in dataloader:
+        images = images.float().to(device)
+        masks = masks.float().to(device)
 
         optimizer.zero_grad()
         outputs = model(images)
-        loss = criterion(outputs, masks.unsqueeze(1))
+        loss = criterion(outputs.squeeze(1), masks)
         loss.backward()
         optimizer.step()
 
         epoch_loss += loss.item()
 
-    print(f'Epoch {epoch+1}, Loss: {epoch_loss/len(train_loader)}')
+    print(f'Epoch {epoch+1}, Loss: {epoch_loss/len(dataloader)}')
 
-# 테스트 데이터셋 및 데이터 로더 생성
-test_dataset = SegmentationDataset(csv_file='./test.csv', transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=4)
+# Load the test data
+test_dataset = SatelliteDataset(csv_file='./test.csv', transform=transform, infer=True)
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=4)
 
-
+# Evaluate the model on the test data
 with torch.no_grad():
     model.eval()
     result = []
-    for images in tqdm(test_loader):
-        images = images.to(device)
+    for images in test_dataloader:
+        images = images.float().to(device)
 
         outputs = model(images)
         masks = torch.sigmoid(outputs).cpu().numpy()
         masks = np.squeeze(masks, axis=1)
-        masks = (masks > 0.35).astype(np.uint8)  # Threshold = 0.35
+        masks = (masks > 0.35).astype(np.uint8)
 
         for i in range(len(images)):
             mask_rle = rle_encode(masks[i])
-            if mask_rle == '':  # 예측된 건물 픽셀이 아예 없는 경우 -1
+            if mask_rle == '':
                 result.append(-1)
             else:
                 result.append(mask_rle)
 
+# Save the predictions to a CSV file
 submit = pd.read_csv('./sample_submission.csv')
 submit['mask_rle'] = result
-
 submit.to_csv('./submit.csv', index=False)
